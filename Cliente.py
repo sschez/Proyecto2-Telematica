9237e3e3fb13fd5f5f410f892bbb283e349934e6
Pascual Gomez Londono <pascual.gomez@globant.com>
2022-05-15 15:43:30 -0500

    Client socket is connecting through http with public hosts

--- Cliente.py
+++ Cliente.py
@@ -1,48 +1,27 @@
-# ********************************************************************************************
-    # Lab: Introduction to sockets
-    # Course: ST0255 - Telemática
-    # TCP-Socket Client
-# ********************************************************************************************
-
-#Import libraries for networking communication...
-
+import chunk
+import re
 import socket
-import constants
 
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def main():
-    print('***********************************')
-    print('Client is running...')
-    client_socket.connect(("127.0.0.1",constants.PORT))
-    local_tuple = client_socket.getsockname()
-    print('Connected to the server from:', local_tuple)
-    print('Enter \"quit\" to exit')
-    print('Input commands:')
-    command_to_send = input()
+#Connect to host
+sock.connect(("www.google.com", 80))
 
-    while command_to_send != constants.QUIT:
-        if command_to_send == '':
-            print('Please input a valid command...')
-            command_to_send = input()     
-        elif (command_to_send == constants.DATA):
-            data_to_send = input('Input data to send: ') 
-            command_and_data_to_send = command_to_send + ' ' + data_to_send
-            client_socket.send(bytes(command_and_data_to_send,constants.ENCONDING_FORMAT))
-            data_received = client_socket.recv(constants.RECV_BUFFER_SIZE)        
-            print(data_received.decode(constants.ENCONDING_FORMAT))
-            command_to_send = input()                                
-        else:
-            client_socket.send(bytes(command_to_send,constants.ENCONDING_FORMAT))
-            data_received = client_socket.recv(constants.RECV_BUFFER_SIZE)        
-            print(data_received.decode(constants.ENCONDING_FORMAT))
-            command_to_send = input()
-    
-    client_socket.send(bytes(command_to_send,constants.ENCONDING_FORMAT))
-    data_received = client_socket.recv(constants.RECV_BUFFER_SIZE)        
-    print(data_received.decode(constants.ENCONDING_FORMAT))
-    print('Closing connection...BYE BYE...')
-    client_socket.close()    
+#Send request
+request = b"GET /images/branding/googlelogo/1x/googlelogo_color_150x54dp.png HTTP/1.1\nHost:www.google.com\r\nConnection: close\r\n\r\n"
+sock.sendall(request)
 
-if __name__ == '__main__':
-    main()
+#Receive response
+response = b""
+while True:
+    chunk = sock.recv(4096)
+    if len(chunk) == 0:
+        break
+    response = response + chunk
+
+#Print response
+
+#print(str(response).encode("utf-8").decode())    #UTF-8-enconding string
+print(response)
+
+sock.close()
